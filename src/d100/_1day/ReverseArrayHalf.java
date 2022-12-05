b1c6dce9334115817be435431eef9e68968c8782
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-05 08:38:44 +0530

    iterating half & swapping

--- src/d100/_1day/ReverseArrayHalf.java
+++ src/d100/_1day/ReverseArrayHalf.java
@@ -1,2 +1,16 @@
-package d100._1day;public class ReverseArrayHalf {
+package d100._1day;
+
+import java.util.Arrays;
+
+public class ReverseArrayHalf {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
