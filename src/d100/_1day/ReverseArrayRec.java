4f14d1cc0efcad3848e475e03b4be6458a86e3a8
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-05 08:39:39 +0530

    using recursion

--- src/d100/_1day/ReverseArrayRec.java
+++ src/d100/_1day/ReverseArrayRec.java
@@ -1,2 +1,21 @@
-package d100._1day;public class ReverseArrayRec {
+package d100._1day;
+
+import java.util.Arrays;
+
+public class ReverseArrayRec {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int start = 0, end = arr.length-1;
+        reverseArrayRec(arr, start, end);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverseArrayRec(int[] arr, int start, int end){
+        if(start>=end)
+            return;
+
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            reverseArrayRec(arr, start+1, end-1);
+    }
 }
