7c83f52422705f1e00d70273d7cedd83a0c3b004
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-11 22:22:44 +0530

    stack and if-else ladder

--- src/leet_code/easy/ValidParantheses.java
+++ src/leet_code/easy/ValidParantheses.java
@@ -1,2 +1,27 @@
-package leet_code.easy;public class ValidParantheses {
+package leet_code.easy;
+
+import java.util.Stack;
+
+public class ValidParantheses {
+    static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else if(st.isEmpty()) return false;
+            else if(st.peek()=='(' && c!=')'
+                    ||st.peek()=='{' && c!='}'
+                    ||st.peek()=='[' && c!=']')  return false;
+            else st.pop();
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}"; //"(]"
+        System.out.println(isValid(s));
+    }
 }
