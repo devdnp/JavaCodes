a79d50e7cda469a803b240b9ae571ce2041a3f19
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-14 22:17:54 +0530

    while 9 increase length

--- src/d100/_5day/PlusOne.java
+++ src/d100/_5day/PlusOne.java
@@ -1,2 +1,30 @@
-package d100._5day;public class PlusOne {
+package d100._5day;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {9,9,9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+        static int[] plusOne(int[] digits) {
+        int n = digits.length-1;
+
+        while (n>=0 && digits[n]==9){
+            n--;
+        }
+        if(n==-1){
+            int[] result = new int[digits.length+1];
+            result[0] = 1;
+            return result;
+        }
+        int[] result = new int[digits.length];
+        result[n] = digits[n] + 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = digits[i];
+        }
+        return result;
+
+    }
 }
+
