8ac44d2bdca4d0ca93c5baee6cbfbd82658d106d
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-15 15:41:55 +0530

    without edge cases

--- src/d100/_5day/BinarySum.java
+++ src/d100/_5day/BinarySum.java
@@ -1,2 +1,17 @@
-package d100._5day;public class BinarySum {
+package d100._5day;
+
+public class BinarySum {
+    static String addBinary(String a, String b) {
+        int one = Integer.parseInt(a, 2);
+        int two = Integer.parseInt(b, 2);
+
+        int sum = one + two;
+        return Integer.toBinaryString(sum);
+    }
+
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1001";
+        System.out.println(addBinary(a, b));
+    }
 }
