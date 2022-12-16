73d3d8080cdf226f47a755a0c66f9ccc7adda81d
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-16 17:59:27 +0530

    formatting

--- src/d100/_4day/CountInversion.java
+++ src/d100/_4day/CountInversion.java
@@ -1,2 +1,23 @@
-package d100._4day;public class CountInversion {
+package d100._4day;
+
+public class CountInversion {
+    public static void main(String[] args) {
+        int[] arr = {1, 20, 6, 4, 5};//{8, 4, 2, 1};
+        System.out.println(countInv(arr));
+    }
+
+    static int countInv(int[] arr){
+        int count = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[i]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
