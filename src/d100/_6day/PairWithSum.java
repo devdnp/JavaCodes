10ce0e5633ea773b07793cac5c40c749eb0ec30f
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-16 18:04:20 +0530

    brute force, nested loops

--- src/d100/_6day/PairWithSum.java
+++ src/d100/_6day/PairWithSum.java
@@ -1,2 +1,23 @@
-package d100._6day;public class PairWithSum {
+package d100._6day;
+
+public class PairWithSum {
+    public static void main(String args[])
+    {
+        int[] arr = { 1, 5, 7, -1, 5 };
+        int sum = 6;
+        System.out.println(getPairsCount(arr, sum));
+    }
+    static int getPairsCount(int[] arr, int sum){
+        int count  = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+//                int result = arr[i]+arr[j];
+//                if (result==sum) ++count;
+                if(arr[i]+arr[j]==sum){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
