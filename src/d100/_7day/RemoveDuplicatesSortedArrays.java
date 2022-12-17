e87af5dbe56f05535c1b6d28686c18d77cc55ada
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-17 22:34:03 +0530

    Not so optimized

--- src/d100/_7day/RemoveDuplicatesSortedArrays.java
+++ src/d100/_7day/RemoveDuplicatesSortedArrays.java
@@ -1,2 +1,23 @@
-package d100._7day;public class RemoveDuplicatesSortedArrays {
+package d100._7day;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesSortedArrays {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        removeDuplicates(nums);
+    }
+    static void removeDuplicates(int[] nums) {
+        int uniqueCount = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueCount] = nums[i];
+                uniqueCount += 1;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
 }
