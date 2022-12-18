8d662b0f0b1559b750bf6b14012c4874b60feebc
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-18 18:54:23 +0530

    using loops

--- src/leet_code/medium/DailyTemperatures.java
+++ src/leet_code/medium/DailyTemperatures.java
@@ -1,2 +1,32 @@
-package leet_code.medium;public class DailyTemperatures {
+package leet_code._daily_prob;
+
+import java.util.Arrays;
+
+public class DailyTemperatures {
+    static int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        Arrays.fill(answer, 0);
+        for (int i = 0; i < temperatures.length-1; i++) {
+            int count = 0;
+            for (int j = i+1; j < temperatures.length; j++) {
+                if(temperatures[i]<temperatures[j]){
+                    count+=1;
+                    break;
+                }
+                else if (temperatures[i]>temperatures[j]){
+                    if(j==temperatures.length-1){count=0; break;}
+                    else count+=1;
+
+                }
+                else count+=0;
+            }
+            answer[i]=count;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+    }
 }
