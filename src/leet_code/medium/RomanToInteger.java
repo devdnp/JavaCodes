b76be6433d7a829b8d4a926f9e028dd9514b06b6
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-20 16:53:36 +0530

    final int & String arrays with StringBuilder

--- src/leet_code/medium/RomanToInteger.java
+++ src/leet_code/medium/RomanToInteger.java
@@ -1,2 +1,21 @@
-package leet_code.medium;public class RomanToInteger {
+package leet_code.medium;
+
+public class RomanToInteger {
+    final static int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    final static String[] rom = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    static String intToRoman(int N) {
+        StringBuilder ans = new StringBuilder();
+            for (int i = 0; N > 0; i++)
+            while (N >= val[i]) {
+                ans.append(rom[i]);
+                N -= val[i];
+            }
+            return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        int num = 1994;
+        System.out.println(intToRoman(num));
+    }
 }
