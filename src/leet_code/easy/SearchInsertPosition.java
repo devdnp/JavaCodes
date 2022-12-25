a0c12113fa4023e85c438de27babf14d529ce0ca
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-25 15:44:06 +0530

    Problem #35

--- src/leet_code/easy/SearchInsertPosition.java
+++ src/leet_code/easy/SearchInsertPosition.java
@@ -1,2 +1,26 @@
-package leet_code.easy;public class SearchInsertPosition {
+package leet_code.easy;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    static int searchInsert(int[] nums, int target) {
+        int f=0;
+        int l=nums.length-1;
+        while(f<=l){
+            int m = (f+l)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(target>nums[m]){
+                f=m+1;
+            }else{
+                l=m-1;
+            }
+        }
+        return l+1;
+    }
 }
