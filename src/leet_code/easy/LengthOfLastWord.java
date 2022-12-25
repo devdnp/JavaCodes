ba86b01229cb6e5313dd1903407504c9370e04b7
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-25 15:44:52 +0530

    Problem #58

--- src/leet_code/easy/LengthOfLastWord.java
+++ src/leet_code/easy/LengthOfLastWord.java
@@ -1,2 +1,15 @@
-package leet_code.easy;public class LengthOfLastWord {
+package leet_code.easy;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";//"Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    static int lengthOfLastWord(String s) {
+        s = s.trim();
+        String word = s.substring(s.lastIndexOf(" ")+1);
+        return word.length();
+
+    }
 }
