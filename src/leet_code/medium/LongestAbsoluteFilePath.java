aa925aa11867659ac34ac6fa8b44136c91036488
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-26 22:42:47 +0530

    Problem #388 stack & dequeue

--- src/leet_code/medium/LongestAbsoluteFilePath.java
+++ src/leet_code/medium/LongestAbsoluteFilePath.java
@@ -1,2 +1,26 @@
-package leet_code.medium;public class LongestAbsoluteFilePath {
+package leet_code.medium;
+//388
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LongestAbsoluteFilePath {
+    public static void main(String[] args) {
+        String input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+        //"dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+        System.out.println(lengthLongestPath(input));
+    }
+    static int lengthLongestPath(String input) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0); // "dummy" length
+        int maxLen = 0;
+        for(String s:input.split("\n")){
+            int lev = s.lastIndexOf("\t")+1; // number of "\t"
+            while(lev+1<stack.size()) stack.pop(); // find parent
+            int len = stack.peek()+s.length()-lev+1; // remove "/t", add"/"
+            stack.push(len);
+            // check if it is file
+            if(s.contains(".")) maxLen = Math.max(maxLen, len-1);
+        }
+        return maxLen;
+    }
 }
