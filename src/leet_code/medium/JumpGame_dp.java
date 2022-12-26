43a348a6d1f9031d149b18c72bf75f05fbef4e40
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-26 22:38:44 +0530

    Problem #55 dynamic programming

--- src/leet_code/medium/JumpGame_dp.java
+++ src/leet_code/medium/JumpGame_dp.java
@@ -1,2 +1,33 @@
-package leet_code.medium;public class JumpGame_dp {
+package leet_code.medium;
+
+import java.util.Arrays;
+
+public class JumpGame_dp {
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};//{3,2,1,0,4};
+        System.out.println(canJump(nums));
+    }
+    static boolean canJump(int[] nums) {
+        boolean[] dp = new boolean[nums.length];
+        Arrays.fill(dp, false);
+        dp[nums.length-1] = true;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] == 0)
+                dp[i] = false;
+            else{
+                if(nums[i] + i >= nums.length)
+                    dp[i] = true;
+                else{
+                    for(int j = 1; j <= nums[i]; j++){
+                        dp[i] = dp[i] || dp[i + j];
+                        //tricky part: the next recurrance is aslo cover some elements for this value so that we can skip them.
+                        j += nums[i + j];
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+
+    }
 }
