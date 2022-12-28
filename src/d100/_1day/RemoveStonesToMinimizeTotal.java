71874ee0c5186517f717fc29eb97c4cb1fbde7fd
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-28 22:54:41 +0530

    Problem #1962 90% efficient

--- src/d100/_1day/RemoveStonesToMinimizeTotal.java
+++ src/d100/_1day/RemoveStonesToMinimizeTotal.java
@@ -1,2 +1,33 @@
-package d100._1day;public class RemoveStonesToMinimizeTotal {
+package d100._1day;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class RemoveStonesToMinimizeTotal {
+    static int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue(Comparator.reverseOrder());
+
+        int sum = 0;
+        for (int pile : piles) {
+            heap.offer(pile);
+            sum+= pile;
+        }
+
+        int removed = 0;
+        while (k != 0) {
+
+            int node = heap.poll();
+            removed += node/2;
+            heap.offer(node - node/2);
+
+
+            k--;
+        }
+        return sum - removed;
+    }
+    public static void main(String[] args) {
+        int[] piles = {4,3,6,7};
+        int k = 3;
+        System.out.println(minStoneSum(piles,k));
+    }
 }
