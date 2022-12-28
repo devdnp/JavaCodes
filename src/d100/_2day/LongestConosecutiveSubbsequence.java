00737bc3f8e57307b2627c5c286e4624b5883907
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-28 23:04:45 +0530

    modified with hashset

--- src/d100/_2day/LongestConosecutiveSubbsequence.java
+++ src/d100/_2day/LongestConosecutiveSubbsequence.java
@@ -1,2 +1,31 @@
-package d100._2day;public class LongestConosecutiveSubbsequence {
+package d100._2day;
+
+import java.util.HashSet;
+
+public class LongestConosecutiveSubbsequence {
+    public static void main(String[] args) {
+        int[] arr= {36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42};//{1, 9, 3, 10, 4, 20, 2};
+        System.out.println(longestConsecSubseq(arr, arr.length));
+    }
+    static int longestConsecSubseq(int[] arr, int n){
+        int count=0;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < n; ++i) {
+            hs.add(arr[i]);
+        }
+        for (int i = 0; i < n; ++i) {
+
+            if(!hs.contains(arr[i]-1)){
+                int j = arr[i];
+                while(hs.contains(j)){
+                    hs.remove(j);
+                    j++;
+                }
+
+                if(count<j-arr[i])
+                    count = j-arr[i];
+            }
+        }
+        return count;
+    }
 }
