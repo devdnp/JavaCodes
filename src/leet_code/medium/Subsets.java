30d9fe82350e14f79edd4be0e5cde4ba943b8ee9
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-30 21:40:36 +0530

    Problem #78

--- src/leet_code/medium/Subsets.java
+++ src/leet_code/medium/Subsets.java
@@ -1,2 +1,26 @@
-package leet_code.medium;public class Subsets {
+package leet_code.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(subsets(nums));
+    }
+    static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtracking(0, nums, result, new ArrayList<>());
+
+        return result;
+    }
+    static void backtracking(int index, int[] nums, List<List<Integer>> result, List<Integer> list) {
+        result.add(new ArrayList<>(list));
+
+        for(int i=index;i<nums.length;i++) {
+            list.add(nums[i]);
+            backtracking(i+1, nums, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
 }
