a354fc8c69e63fa1239d0a5fb423dcd278c68e7b
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-02 12:38:16 +0530

    Problem #520

--- src/leet_code/easy/DetectCapital.java
+++ src/leet_code/easy/DetectCapital.java
@@ -1,2 +1,14 @@
-package leet_code.easy;public class DetectCapital {
+package leet_code.easy;
+
+public class DetectCapital {
+    public static void main(String[] args) {
+        String word="JavaProgramming";//""Java";
+        System.out.println(detectCapitalUse(word));
+    }
+    static boolean detectCapitalUse(String word) {
+        if(word.toLowerCase().equals(word)) return true;
+        else if(word.toUpperCase().equals(word)) return true;
+        else if((word.substring(0,1).toUpperCase()+word.toLowerCase().substring(1,word.length())).equals(word)) return true;
+        return false;
+    }
 }
