19d00b0c2f0b136fe450556c0c213bce8d20e41a
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-02 12:39:09 +0530

    Problem #520 Regex

--- src/d100/_2day/DetectCapital.java
+++ src/d100/_2day/DetectCapital.java
@@ -1,2 +1,11 @@
-package d100._2day;public class DetectCapital {
+package d100._2day;
+
+public class DetectCapital {
+    public static void main(String[] args) {
+        String word = "FlaG";
+        System.out.println(detectCapitalUse(word));
+    }
+    static boolean detectCapitalUse(String word) {
+        return word.matches("[A-Z]*|.[a-z]*");
+    }
 }
