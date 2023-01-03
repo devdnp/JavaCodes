480eb522a5561f2685dd514ee5f5096aac1bf7f0
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-03 23:11:09 +0530

    Problem #944

--- src/leet_code/easy/DeleteColumnsToMakeSorted.java
+++ src/leet_code/easy/DeleteColumnsToMakeSorted.java
@@ -1,2 +1,23 @@
-package leet_code.easy;public class DeleteColumnsToMakeSorted {
+package leet_code.easy;
+
+public class DeleteColumnsToMakeSorted {
+    static int minDeletionSize(String[] strs) {
+        int K = strs[0].length();
+
+        int answer = 0;
+        for (int col = 0; col < K; col++) {
+            for (int row = 1; row < strs.length; row++) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"cba","daf","ghi"};
+        System.out.println(minDeletionSize(strs));
+    }
 }
