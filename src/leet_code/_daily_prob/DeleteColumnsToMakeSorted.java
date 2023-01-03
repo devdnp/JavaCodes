1f5c4943c596d30bfda1a7813828e0165d6f6cc8
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-03 23:09:54 +0530

    Problem #944

--- src/leet_code/_daily_prob/DeleteColumnsToMakeSorted.java
+++ src/leet_code/_daily_prob/DeleteColumnsToMakeSorted.java
@@ -1,2 +1,31 @@
-package leet_code._daily_prob;public class DeleteColumnsToMakeSorted {
+package leet_code._daily_prob;
+
+public class DeleteColumnsToMakeSorted {
+    public static void main(String[] args) {
+        String[] strs = {"zyx","wvu","tsr"};
+        System.out.println(minDeletionSize(strs));
+    }
+    static int minDeletionSize(String[] A) {
+        int deleted = 0;
+        if (A == null || A.length == 0) return deleted;
+        int rowSize = A[0].length();
+
+        for (int col = 0; col < rowSize; col++){
+            if (!isSorted(A, col)) deleted++;
+        }
+
+        return deleted;
+    }
+
+    static boolean isSorted(String[] A, int col){
+        int N = A.length;
+        char prev = (char)0;
+        for (int i = 0; i < N; i++){
+            char c = A[i].charAt(col);
+            if (c < prev) return false;
+            prev = c;
+        }
+
+        return true;
+    }
 }
