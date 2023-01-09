7110cb8c3c2f1dfb0072e24d0c53388e8e53a786
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-09 23:12:50 +0530

    if-else & temp

--- src/gfg/_daily_problem/ContainerWithMostWater.java
+++ src/gfg/_daily_problem/ContainerWithMostWater.java
@@ -1,30 +1,16 @@
 package gfg._array._daily_problem;
 
 import java.io.*;
-import java.util.*;
 import java.lang.*;
 
 
 public class ContainerWithMostWater {
     public static void main (String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-        while(t-->0){
-            int n = Integer.parseInt(br.readLine()); // input size of array
-            int arr[] = new int[n];
-            String inputLine[] = br.readLine().trim().split("\\s+");
-            for(int i=0; i<n; i++){
-                arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
-            }
-            Solve T = new Solve();
-            long ans = T.maxArea(arr,n);
-            System.out.println(ans);
-        }
+        int[] a = {1,5,4,3};
+        int len = a.length;
+        System.out.println(maxArea(a, len));
     }
-}
-class Solve{
-
-    long maxArea(int a[], int len){
+    static long maxArea(int a[], int len){
         // Code Here
         int res=0,temp=0;
         int i=0,j=len-1;
