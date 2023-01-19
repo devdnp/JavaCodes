ca65c00f59500b3479610e5653ca644d9cda2c0d
durgeshvipul <durgeshpandey419@gmail.com>
2023-01-19 23:56:57 +0530

    Problem #974 simple

---
+++ src/leet_code/medium/SubarrayWithSUmEqualToK.java
@@ -0,0 +1,28 @@
+package leet_code.medium;
+//974
+public class SubarrayWithSUmEqualToK {
+    public static void main(String[] args) {
+        int[] nums = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subarraysDivByK(nums,k));
+    }
+    static int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int prefixMod = 0, result = 0;
+
+        // There are k mod groups 0...k-1.
+        int[] modGroups = new int[k];
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+}
