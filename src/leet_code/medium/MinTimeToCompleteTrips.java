afeda34962343779250afb5c0f496048239ff941
durgeshvipul <durgeshpandey419@gmail.com>
2023-03-07 23:20:31 +0530

    Problem #2187

---
+++ src/leet_code/medium/MinTimeToCompleteTrips.java
@@ -0,0 +1,21 @@
+package leet_code.medium;
+//2187
+
+public class MinTimeToCompleteTrips {
+    public long minimumTime(int[] time, int totalTrips) {
+        long lo = 0, hi = 100000000000000L;
+        while(lo<hi){
+            long req=(lo+hi)/2;
+            long trips = 0;
+            for (int t : time) {
+                trips += req / t;
+            }
+            if (trips < totalTrips) {
+                lo = req + 1;
+            }else {
+                hi = req;
+            }
+        }
+        return lo;
+    }
+}
