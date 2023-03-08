1c7d5fa692f48182014fa9819fac5b279ab347d8
durgeshvipul <durgeshpandey419@gmail.com>
2023-03-08 22:30:50 +0530

    Problem #875

---
+++ src/leet_code/medium/KokoEatingBananas.java
@@ -0,0 +1,25 @@
+package leet_code.medium;
+//875
+public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low= 1;
+        int hi = 1000000000;
+
+        while(low< hi){
+            int mid = low + (hi - low) / 2;
+            if(canEatInTime(piles, mid, h)) hi = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+    public boolean canEatInTime(int piles[], int k, int h){
+        int time = 0;
+        for(int p : piles){
+            time+=(p-1)/k+1;
+            if(time>h){
+                break;
+            }
+        }
+        return time <= h;
+    }
+}
