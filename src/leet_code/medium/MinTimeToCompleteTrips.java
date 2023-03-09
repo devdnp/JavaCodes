e3f721c47752ea495c294677ff657088b809e7be
durgeshvipul <durgeshpandey419@gmail.com>
2023-03-09 22:35:09 +0530

    Problem #2187

--- src/leet_code/medium/MinTimeToCompleteTrips.java
+++ src/leet_code/medium/MinTimeToCompleteTrips.java
@@ -2,7 +2,13 @@ package leet_code.medium;
 //2187
 
 public class MinTimeToCompleteTrips {
-    public long minimumTime(int[] time, int totalTrips) {
+
+    public static void main(String[] args) {
+        int[] time = {1,2,3};
+        int totalTrips = 5;
+        System.out.println(minimumTime(time,totalTrips));
+    }
+    public static long minimumTime(int[] time, int totalTrips) {
         long lo = 0, hi = 100000000000000L;
         while(lo<hi){
             long req=(lo+hi)/2;
