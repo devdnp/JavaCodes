f6784802aa6721375e411babc734d7b8d8a059a5
durgeshvipul <durgeshpandey419@gmail.com>
2023-03-11 12:17:03 +0530

    Updated with dataset 3

--- src/leet_code/_daily_prob/KthMissingPositiveNumber.java
+++ src/leet_code/_daily_prob/KthMissingPositiveNumber.java
@@ -2,8 +2,8 @@ package leet_code._daily_prob;
 //Problem 1539
 public class KthMissingPositiveNumber {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,7,11};
-        int k = 5;
+        int[] arr = {5,4,9,26,13};//{2,3,4,7,11};
+        int k = 3;//5;
         System.out.println(findKthPositive(arr,k));
     }
     public static int findKthPositive(int[] arr, int k) {
