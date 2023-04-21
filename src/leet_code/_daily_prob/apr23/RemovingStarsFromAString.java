18f25c8ec4f6c7e6771176252db643a568a899d0
durgeshvipul <durgeshpandey419@gmail.com>
2023-04-21 22:52:56 +0530

    Problem #2390 - 11Apr

---
+++ src/leet_code/_daily_prob/apr23/RemovingStarsFromAString.java
@@ -0,0 +1,19 @@
+package leet_code._daily_prob.apr23;
+//11Apr
+//#2390
+public class RemovingStarsFromAString {
+
+    public static void main(String[] args) {
+        String s = "leet**cod*e";
+        System.out.println(removeStars(s));
+    }
+    public static String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (final char c : s.toCharArray())
+            if (c == '*')
+                sb.deleteCharAt(sb.length() - 1);
+            else
+                sb.append(c);
+        return sb.toString();
+    }
+}
