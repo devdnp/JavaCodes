ed07bad944faf17618c891e866f714d1cd7165db
durgeshvipul <durgeshpandey419@gmail.com>
2023-05-16 23:44:04 +0530

    Problem #24 16-May-23

---
+++ src/leet_code/_daily_prob/may23/SwapNodesInPairs.java
@@ -0,0 +1,30 @@
+package leet_code._daily_prob.may23;
+//16-May-2023
+//#24
+public class SwapNodesInPairs {
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//    public ListNode swapPairs(ListNode head) {
+//        if(head == null || head.next == null)return head;
+//        ListNode d = new ListNode(0), a;
+//        d.next = head;
+//        a=d;
+//        while(head!=null&&head.next!=null) {
+//            a.next = head.next;
+//            head.next = head.next.next;
+//            a.next.next = head;
+//            a = a.next.next;
+//            head =  head.next;
+//        }
+//        return d.next;
+//    }
+}
