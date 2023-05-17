0e1eb7ea77c3c40a5623d3b2d58f3d8db71b6b10
durgeshvipul <durgeshpandey419@gmail.com>
2023-05-17 16:09:30 +0530

    Problem #2130 17-May-23

---
+++ src/leet_code/_daily_prob/may23/MaxTwinSumOfLinkedList.java
@@ -0,0 +1,32 @@
+package leet_code._daily_prob.may23;
+//17-05-2023
+//#2130
+public class MaxTwinSumOfLinkedList {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//    public int pairSum(ListNode head) {
+//        ListNode slow=head,fast=head,pre=null,curr=head;
+//        int ans=0;
+//        while(fast!=null){
+//            fast=fast.next.next;
+//            curr=slow;
+//            slow=slow.next;
+//            curr.next=pre;
+//            pre=curr;
+//        }
+//        while(slow!=null){
+//            ans=Math.max(ans,pre.val+slow.val);
+//            slow=slow.next;
+//            pre=pre.next;
+//        }
+//        return ans;
+//    }
+}
